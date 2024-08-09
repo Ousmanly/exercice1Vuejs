5eae5c16cc7ef7a7dd1d70e447aaefbeac92fbc3
Ousmanly <lyousmane492@gmail.com>
2024-08-09 11:19:49 +0200

    added files

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<ListeEmployes :employes="employes"/>
+</template>
+<script setup>
+import { ref } from 'vue';
+import ListeEmployes from './components/listeEmployes.vue';
+const employes = ref([
+  {id:1, prenom:"Ousmane", nom:"Ly"},
+  {id:1, prenom:"Omar", nom:"Ndiaye"},
+  {id:1, prenom:"Abdarahmane", nom:"Thimbo"},
+  {id:1, prenom:"Assa", nom:"Bradji"},
+])
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/listeEmployes.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <h1>Liste des employes</h1>
+    <ol>
+      <li v-for="employe in employes" :key="employe.id" @mousemove="MouseOver" @mouseleave="MouseLeave" >
+        {{ employe.prenom }} {{ employe.nom }}
+      </li>
+    </ol>
+  </section>
+</template>
+
+<script setup>
+const props = defineProps({
+  employes:Array
+})
+
+function MouseOver(event) {
+  event.target.style.color="blue";
+  event.target.style.backgroundColor= "aliceblue";
+}
+function MouseLeave(event) {
+  event.target.style.color="";
+  event.target.style.backgroundColor= "";
+}
+</script>
+
+<style scoped>
+  li{
+    color: rgb(108, 108, 199);
+    width: 200px;
+    padding: 7px;
+  }
+  section{
+    width: fit-content;
+    margin: 100px auto;
+  }
+</style>
